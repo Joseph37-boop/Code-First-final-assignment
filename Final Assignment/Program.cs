fe3fb17aeb30600b744144778d7b6a5f39fed027
Joseph37-boop <127780142+Joseph37-boop@users.noreply.github.com>
2023-10-12 17:44:28 -0600

    Add project files.

---
+++ Final Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Grade { get; set; }
+}
+
+
+
+public class StudentContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new StudentContext())
+        {
+            context.Database.EnsureCreated();
+
+            var newStudent = new Student
+            {
+                Name = "Joseph",
+                Grade = 95.5
+            };
+
+            context.Students.Add(newStudent);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added to the database.");
+        }
+    }
+}
+
